1db4e13b3bc6922b10800ece9a0ea582780c99d5
JustN00ne <justnoone963@gmail.com>
2026-05-16 20:32:18 +0200

    Postcss update

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
-// postcss.config.js
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {}, // <-- This is the crucial fix
-    autoprefixer: {},
-  },
-}
+const config = {
+    plugins: {
+        tailwindcss: {},
+    },
+};
+
+export default config;
